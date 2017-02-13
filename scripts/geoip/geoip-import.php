7b216b87b9aa56d19553ce62147a462a20f3926c
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2017-02-12 23:04:46 -0500

    geoip import updated

--- scripts/geoip/geoip-import.php
+++ scripts/geoip/geoip-import.php
@@ -1,6 +1,7 @@
 <?php
 // this should be a cronjob that runs once a day or week or whenever
 // php /var/jetendo-server/jetendo/scripts/geoip/geoip-import.php
+// the main import only takes 3 to 10 minutes to run, but then the prefix optimization at the end takes about 30 minutes to finish
 require(get_cfg_var("jetendo_scripts_path")."library.php");
 set_time_limit(5000);
 $mysqlUser=get_cfg_var("jetendo_mysql_default_username");
@@ -15,7 +16,7 @@ if(!is_dir($path)){
 chdir($path);
 
 $cmysql=new mysqli(get_cfg_var("jetendo_mysql_default_host"),get_cfg_var("jetendo_mysql_default_user"), get_cfg_var("jetendo_mysql_default_password"), "task"); // zGetDatasource() 
-
+/**/
 echo "Drop geoip_block_safe\n";
 $sql="DROP TABLE IF EXISTS `geoip_block_safe` ";
 $r=$cmysql->query($sql, MYSQLI_STORE_RESULT); 
@@ -32,19 +33,22 @@ if($r===FALSE){
 
 $sql="CREATE TABLE IF NOT EXISTS `geoip_block` (
   `geoip_block_network_cidr` varchar(32) NOT NULL DEFAULT '',
-  `geoip_location_id` int(11) DEFAULT NULL,
-  `geoip_block_registered_country_geoname_id` int(11) DEFAULT NULL,
-  `geoip_block_represented_country_geoname_id` int(11) DEFAULT NULL,
-  `geoip_block_is_anonymous_proxy` varchar(1) DEFAULT '0',
-  `geoip_block_is_satellite_provider` varchar(1) DEFAULT '0',
-  `geoip_block_postal_code` varchar(45) DEFAULT NULL,
-  `geoip_block_latitude` float DEFAULT NULL,
-  `geoip_block_longitude` float DEFAULT NULL,
-  `geoip_block_network` int(10) unsigned DEFAULT NULL,
-  `geoip_block_broadcast` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`geoip_block_network_cidr`),
-  KEY `newindex1` (`geoip_block_network`,`geoip_block_broadcast`) 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+  `geoip_location_id` int(11) unsigned NOT NULL,
+  `geoip_block_registered_country_geoname_id` int(11) unsigned NOT NULL,
+  `geoip_block_represented_country_geoname_id` int(11) unsigned NOT NULL,
+  `geoip_block_is_anonymous_proxy` varchar(1) NOT NULL DEFAULT '0',
+  `geoip_block_is_satellite_provider` varchar(1) NOT NULL DEFAULT '0',
+  `geoip_block_postal_code` varchar(45) NOT NULL,
+  `geoip_block_latitude` float NOT NULL,
+  `geoip_block_longitude` float NOT NULL,
+  `geoip_block_prefix` int(10) unsigned NOT NULL DEFAULT '0',
+  `geoip_block_network` int(10) unsigned NOT NULL,
+  `geoip_block_broadcast` int(10) unsigned NOT NULL,
+   `geoip_block_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`geoip_block_id`),
+  KEY `newindex1` (`geoip_block_network`,`geoip_block_broadcast`),
+  KEY `newindex2` (`geoip_block_prefix`,`geoip_block_network`,`geoip_block_broadcast`)
+) ENGINE=InnoDB AUTO_INCREMENT=2942433 DEFAULT CHARSET=utf8";
 $r=$cmysql->query($sql, MYSQLI_STORE_RESULT); 
 echo "Create geoip_block\n";
 if($r===FALSE){
@@ -76,19 +80,22 @@ if($r===FALSE){
 
 $sql="CREATE TABLE `geoip_block_safe` (
   `geoip_block_network_cidr` varchar(32) NOT NULL DEFAULT '',
-  `geoip_location_id` int(11) DEFAULT NULL,
-  `geoip_block_registered_country_geoname_id` int(11) DEFAULT NULL,
-  `geoip_block_represented_country_geoname_id` int(11) DEFAULT NULL,
-  `geoip_block_is_anonymous_proxy` varchar(1) DEFAULT '0',
-  `geoip_block_is_satellite_provider` varchar(1) DEFAULT '0',
-  `geoip_block_postal_code` varchar(45) DEFAULT NULL,
-  `geoip_block_latitude` float DEFAULT NULL,
-  `geoip_block_longitude` float DEFAULT NULL,
-  `geoip_block_network` int(10) unsigned DEFAULT NULL,
-  `geoip_block_broadcast` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`geoip_block_network_cidr`),
-  KEY `newindex1` (`geoip_block_network`,`geoip_block_broadcast`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+  `geoip_location_id` int(11) unsigned NOT NULL,
+  `geoip_block_registered_country_geoname_id` int(11) unsigned NOT NULL,
+  `geoip_block_represented_country_geoname_id` int(11) unsigned NOT NULL,
+  `geoip_block_is_anonymous_proxy` varchar(1) NOT NULL DEFAULT '0',
+  `geoip_block_is_satellite_provider` varchar(1) NOT NULL DEFAULT '0',
+  `geoip_block_postal_code` varchar(45) NOT NULL,
+  `geoip_block_latitude` float NOT NULL,
+  `geoip_block_longitude` float NOT NULL,
+  `geoip_block_prefix` int(10) unsigned NOT NULL DEFAULT '0',
+  `geoip_block_network` int(10) unsigned NOT NULL,
+  `geoip_block_broadcast` int(10) unsigned NOT NULL,
+  `geoip_block_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`geoip_block_id`),
+  KEY `newindex1` (`geoip_block_network`,`geoip_block_broadcast`),
+  KEY `newindex2` (`geoip_block_prefix`,`geoip_block_network`,`geoip_block_broadcast`)
+) ENGINE=InnoDB AUTO_INCREMENT=2942433 DEFAULT CHARSET=utf8";
 $r=$cmysql->query($sql, MYSQLI_STORE_RESULT); 
 if($r===FALSE){
 	zEmailErrorAndExit("GeoIP Import Failed", "Failed: ".$cmysql->error);
@@ -156,7 +163,8 @@ if($r===FALSE){
 }
 
 echo "Process blocks\n";
-$sql="update geoip_block_safe set 
+$sql="update geoip_block_safe set  
+geoip_block_prefix = (substring_index(substring_index(geoip_block_network_cidr, '.', 1), '.', -1)*256)+substring_index(substring_index(geoip_block_network_cidr, '.', 2), '.', -1),
 geoip_block_broadcast = (INET_ATON(substring_index(geoip_block_network_cidr, '/', 1)) + (pow(2, (32-substr(geoip_block_network_cidr, instr(geoip_block_network_cidr, '/')+1)))-1)), 
 geoip_block_network = inet_aton(substring_index(geoip_block_network_cidr, '/', 1))";
 $r=$cmysql->query($sql, MYSQLI_STORE_RESULT); 
@@ -164,6 +172,65 @@ if($r===FALSE){
 	zEmailErrorAndExit("GeoIP Import Failed", "Failed: ".$cmysql->error);
 }
 
+$sql="SELECT * FROM geoip_block_safe group by geoip_block_prefix";
+$r=$cmysql->query($sql, MYSQLI_STORE_RESULT); 
+if($r===FALSE){
+	zEmailErrorAndExit("GeoIP Prefix Query Failed", "Failed: ".$cmysql->error);
+}
+$blockLookup=array();
+while($row=$r->fetch_object()){
+	$blockLookup[$row->geoip_block_prefix]=true;
+}
+for($i=0;$i<=256;$i++){
+	echo "Process missing blocks for ".$i.".0.0.0\n";
+	for($n=0;$n<=256;$n++){
+		$prefix=($i*256)+$n;
+		if(isset($blockLookup[$prefix])){
+			continue;
+		}
+		// lookup ip block
+		$ip=$i.".".$n.".0.0"; 
+		
+		$blockLookup[$prefix]=true;
+		
+		$sql="SELECT * 
+		FROM geoip_block_safe FORCE INDEX(newindex2)
+		where  
+		geoip_block_prefix < '".$prefix."' AND
+		inet_aton('".$cmysql->real_escape_string($ip)."') >= geoip_block_network
+		AND inet_aton('".$cmysql->real_escape_string($ip)."') <= geoip_block_broadcast 
+		LIMIT 0,1";
+		$r2=$cmysql->query($sql, MYSQLI_STORE_RESULT); 
+		if($r2===FALSE){
+			zEmailErrorAndExit("GeoIP Lookup Query Failed", "Failed: ".$cmysql->error);
+		}
+		
+		// ignore missing blocks because they are not valid ips.
+		while($row2=$r2->fetch_object()){
+			$sql="INSERT INTO geoip_block_safe
+			SET 
+			geoip_block_network_cidr='$row2->geoip_block_network_cidr',
+			geoip_location_id='$row2->geoip_location_id',
+			geoip_block_registered_country_geoname_id='$row2->geoip_block_registered_country_geoname_id',
+			geoip_block_represented_country_geoname_id='$row2->geoip_block_represented_country_geoname_id',
+			geoip_block_is_anonymous_proxy='$row2->geoip_block_is_anonymous_proxy',
+			geoip_block_is_satellite_provider='$row2->geoip_block_is_satellite_provider',
+			geoip_block_postal_code='$row2->geoip_block_postal_code',
+			geoip_block_latitude='$row2->geoip_block_latitude',
+			geoip_block_longitude='$row2->geoip_block_longitude',
+			geoip_block_prefix='$prefix',
+			geoip_block_network='$row2->geoip_block_network',
+			geoip_block_broadcast='$row2->geoip_block_broadcast'";
+			$r3=$cmysql->query($sql, MYSQLI_STORE_RESULT);  
+			if($r3===FALSE){
+				zEmailErrorAndExit("GeoIP Prefix Insert Query Failed", "Failed: ".$cmysql->error);
+			}
+			//var_dump($r3);			echo $sql;			exit;
+		}
+	}
+}
+echo("done");
+exit;
 echo "Swap geoip tables\n";
 $sql="RENAME TABLE `geoip_location_safe` to `geoip_location_temp`, `geoip_location` to `geoip_location_safe`, `geoip_location_temp` to `geoip_location`,  `geoip_block_safe` to `geoip_block_temp`, `geoip_block` to `geoip_block_safe`, `geoip_block_temp` to `geoip_block` ";
 $r=$cmysql->query($sql, MYSQLI_STORE_RESULT); 
@@ -171,23 +238,5 @@ if($r===FALSE){
 	zEmailErrorAndExit("GeoIP Import Failed", "Failed: ".$cmysql->error);
 }
 		
-echo('done');exit;
-/*
-
-$ip='50.88.30.86';
-//$ip2='205.251.199.158';
-
-// this query is up to 10 times faster then the single index version
-$cmd='SELECT SQL_NO_CACHE  
- geoip_location.geoip_location_country_name,
- geoip_location.geoip_location_city_name,
- geoip_block.geoip_block_latitude,
- geoip_block.geoip_block_longitude 
-FROM geoip_block FORCE INDEX(newindex1), geoip_location  
-where geoip_block.geoip_block_id = geoip_location.geoip_block_id 
-AND inet_aton('.$cmysql->real_escape_string($ip).') >= geoip_block_network
-AND inet_aton('.$cmysql->real_escape_string($ip).') <= geoip_block_broadcast';
-
-*/
-
+echo('done');exit; 
 ?>
