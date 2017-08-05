c976028542f49c23e577dfe50b073a96ada63153
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2017-08-05 17:08:25 -0400

    imap progress

--- scripts/email/imap.php
+++ scripts/email/imap.php
@@ -3,19 +3,51 @@
 // php /var/jetendo-server/jetendo/scripts/email/imap.php > /var/jetendo-server/custom-secure-scripts/imap-images/email-result.html
 require("/var/jetendo-server/jetendo/scripts/library.php"); 
 require("zMailClient.php"); 
-require("/var/jetendo-server/custom-secure-scripts/email-config.php");
+// no longer needed:
+//require("/var/jetendo-server/custom-secure-scripts/email-config.php");
  
 function microtimeFloat()
 {
     list($usec, $sec) = explode(" ", microtime());
     return ((float)$usec + (float)$sec);
+} 
+
+function processEmailAddressList($list, $accountEmail){
+
+	$arrList=explode(",", $list);
+	$arrEmail=array();
+	for($i=0;$i<count($arrList);$i++){
+		$arrCurrentList=explode("<", $arrList[$i]);
+		if(count($arrCurrentList) == 2){
+			$arrEnd=explode(">", $arrCurrentList[1]);
+			$temp=array(
+				"name"=>trim($arrCurrentList[0]),
+				"email"=>trim($arrEnd[0])
+			));
+		}else{ 
+			$temp=array(
+				"name"=>"",
+				"email"=>trim($arrList[$i])
+			));
+		}
+ 
+		$arrPart1=explode("@", $temp["email"]);
+		if(count($arrPart1) <= 1){
+			continue;
+		}
+		$arrPart2=explode("+", $arrPart1[0]);
+		if(count($arrPart2)==2){
+			$tempEmail=$arrPart2[0]."@".$arrPart1[1];
+			if($tempEmail==$accountEmail){
+				$temp["plusId"]=$arrPart2[1];
+				$temp["originalEmail"]=$temp["email"];
+				$temp["email"]=$tempEmail;
+			}
+		}
+		array_push($arrEmail, $temp);
+	}
+	return $arrEmail;
 }
-
-
-
-// TODO: must store images in site specific locations
-$filePath="/var/jetendo-server/custom-secure-scripts/imap-images/";
-
 set_time_limit(120);
 $timeout=55; // seconds
 $timeStart=microtimeFloat();
@@ -35,7 +67,7 @@ $sitesWritablePath=get_cfg_var("jetendo_sites_writable_path");
 $arrAccount=array();
 $testDomain=get_cfg_var("jetendo_test_domain"); 
 while($account=$r->fetch_array(MYSQLI_ASSOC)){
-	$thedomainpath=str_replace("www.", "", str_replace(".".$testDomain, "", $account->site_short_domain));
+	$thedomainpath=str_replace("www.", "", str_replace(".".$testDomain, "", $account["site_short_domain"]));
 	$sitePath=$sitesWritablePath.str_replace(".","_",$thedomainpath)."/zuploadsecure/email-attachments/";
 	if(!is_dir($sitePath)){
 		mkdir($sitePath, 0770);
@@ -71,33 +103,76 @@ while($account=$r->fetch_array(MYSQLI_ASSOC)){
 	/*$arrMessage=$myMail->listMessagesSinceMessage(2);
 	var_dump($arrMessage);
 	exit;*/
-	$arrMessage=$myMail->listMessages(4); // TODO: delete number when done    // test 4 which has no attachments.  and test plain text only email and html only email
+	$arrMessage=$myMail->listMessages(5); // TODO: delete number when done    // test 4 which has no attachments.  and test plain text only email and html only email
 	foreach($arrMessage as $msgId=>$msg){ 
 		// queue message to be downloaded individually in queue_pop table
-		echo('<h2>Downloading email #'.$msgId.' parsed plus address:'.$msg['plusId'].'</h2>');
+		//echo('<h2>Downloading email #'.$msgId.' parsed plus address:'.$msg['plusId'].'</h2>');
 		$message=$myMail->getFullMessage($msgId);  
-		echo(json_encode($message,  JSON_FORCE_OBJECT | JSON_PRETTY_PRINT));
-		exit;
+		/*echo(json_encode($message,  JSON_FORCE_OBJECT | JSON_PRETTY_PRINT));
+		echo("\n");
+		echo($sitePath);
+		exit;*/
   
 		$subject="";
 		if(isset($message["headers"]["parsed"]["Subject"])){
 			$subject=$message["headers"]["parsed"]["Subject"];
-		}
-		/*
-		// TODO need function to parse: Name <email>
-
-		// need to add FROM/TO/CC/BCC as parsed arrays to the JSON object
+		}  
+		$from=array();
 		$to=array();
-		
-		array_push($to, array(
-			name=>"First Last",
-			email=>"someone@somewhere.com"
-		));
-		*/
-
-		if(!isset($message['plusId'])){
-			$message['plusId']='';
+		$cc=array();
+		$bcc=array(); 
+		if(isset($message["headers"]["parsed"]["From"])){
+			$tempFrom=processEmailAddressList($message["headers"]["parsed"]["From"]);
+			$from=$tempFrom[0];
 		}
+		if(isset($message["headers"]["parsed"]["To"])){
+			$to=processEmailAddressList($message["headers"]["parsed"]["To"], $message, $account["imap_account_username"]);
+		}
+		if(isset($message["headers"]["parsed"]["Cc"])){
+			$cc=processEmailAddressList($message["headers"]["parsed"]["Cc"]);
+		}
+		if(isset($message["headers"]["parsed"]["Bcc"])){
+			$bcc=processEmailAddressList($message["headers"]["parsed"]["Bcc"]);
+		}  
+
+		$message['plusId']='';
+
+		if(isset($message["headers"]["parsed"]["Delivered-To"])){
+			$deliveredTo=processEmailAddressList($message["headers"]["parsed"]["Delivered-To"]);
+			if(isset($deliveredTo["plusId"])){
+				$message['plusId']=$deliveredTo["plusId"];
+			}
+		}
+		if($message['plusId']==''){
+			// find the plus id in the to address list instead.
+
+			for($i=0;$i<count($to);$i++){
+				// check for address to match 
+				if(isset($to[$i]["plusId"])){
+					if($to[$i]["email"] == $account["imap_account_username"]){
+						// set plusId
+						$message['plusId']=$to[$i]["plusId"];
+						break;
+					}
+				}
+			}
+		}
+		if($message['plusId']==''){
+			// find the plus id in the cc address list instead.
+			for($i=0;$i<count($cc);$i++){
+				// check for address to match 
+				if(isset($cc[$i]["plusId"])){
+					if($cc[$i]["email"] == $account["imap_account_username"]){
+						// set plusId
+						$message['plusId']=$cc[$i]["plusId"];
+						break;
+					}
+				}
+			}
+		}
+		// if the plusId is still missing, then this email must have been sent directly to this email address without a plus id.
+
+
 		if(!isset($message['html'])){
 			$message['html']='';
 		}
@@ -115,72 +190,49 @@ while($account=$r->fetch_array(MYSQLI_ASSOC)){
 		}
 
 		$arrFile=array();
+		$emailSize=0;
 		foreach($message['attachments'] as $key=>$file){
+			$emailSize+=$file["size"];
             $tempFile=array(
             	"size"=>$file["size"], //: 292427,
 	            "filePath"=>$file["path"], // "\/var\/jetendo-server\/custom-secure-scripts\/imap-images\/cbenckacbifelmnn1.png",
             	"fileName"=> $file["name"]
             ); 
 			array_push($arrFile, $tempFile);
-		}
-		var_dump($message);exit; 
-		$arrData=array( 
-			"headers" => $message["headers"],
-			"subject" => $subject,
-			"html" => $message['html'],
-			"text" => $message['text'],
-			"files" => $arrFile,
-			"size" => $emailSize
-		);
-		echo('<h2>Plain Text:</h2><pre>'.$message['text'].'</pre>'."<hr>");
-		echo('<h2>HTML Text:</h2>'.$message['html']."<hr>"); 
-		// TODO figure out why message 5 is missing word doc
-		foreach($message['attachments'] as $key=>$val){
-			echo('Attachment #'.$key.': '.$val['name']."<hr>");
-		}
-		// move files to zuploadsecure/email/ - size could grow too large
-		// each site will have a subdomain at our mail routing domain?  or leverage the actual domain via SMTP with separate configuration for each client.
-		// $sitesWritablePath
+		} 
+		//var_dump($message);exit; 
+		$dataObj=new stdClass();
+		$dataObj->headers=$message["headers"];
+		$dataObj->from=$from;
+		$dataObj->to=$to;
+		$dataObj->cc=$cc;
+		$dataObj->bcc=$bcc;
+		$dataObj->subject=$subject;
+		$dataObj->html=$message['html'];
+		$dataObj->text=$message['text'];
+		$dataObj->files=$arrFile;
+		
+		$emailSize+=strlen($message["headers"]["raw"]);
+		$emailSize+=strlen($dataObj->html);
+		$emailSize+=strlen($dataObj->text);
 
-		// store message in queue_pop 
+		$dataObj->size=$emailSize;
 
-		json_encode($arrData, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+		$messageJson=json_encode($dataObj,  JSON_PRETTY_PRINT); 
+		echo($messageJson);		exit; 
 
-/*
-
-queue_pop_file_json=[{
-	originalFileName:'originalFileName.jpg',
-	filePath:'relative/path/to/hashedFileName.jpg',
-	size:'1230',
-},
-...other files
-]
-
-// maybe a field for plus id, or part of a json object.
-
-$site_id=1;
-$sql="INSERT INTO queue_pop SET 
-site_id='".$cmysql->real_escape_string($site_id).',
-queue_pop_message_uid='".$cmysql->real_escape_string($msg['uid']).',
-queue_pop_created_datetime='".$cmysql->real_escape_string(date('Y-m-d H:i:s')).',
-queue_pop_updated_datetime='".$cmysql->real_escape_string(date('Y-m-d H:i:s')).',
-queue_pop_last_run_datetime='".$cmysql->real_escape_string().',
-queue_pop_header_data='".$cmysql->real_escape_string().',
-queue_pop_subject='".$cmysql->real_escape_string($message['subject']).',
-queue_pop_body_text='".$cmysql->real_escape_string($message['text']).',
-queue_pop_body_html='".$cmysql->real_escape_string($message['html']).',
-queue_pop_file_json='".$cmysql->real_escape_string().',
-queue_pop_fail_count='".$cmysql->real_escape_string().',
-queue_pop_response='".$cmysql->real_escape_string().',
-queue_pop_deleted='".$cmysql->real_escape_string(0).' ';
-$cmysql->query($sql, MYSQLI_STORE_RESULT);
-*/
-/* 
-these don't make sense
-queue_pop_return_p='".$cmysql->real_escape_string().',
-queue_pop_timeout='".$cmysql->real_escape_string().',
-queue_pop_retry_interval='".$cmysql->real_escape_string().',
-*/
+		$sql="INSERT INTO queue_pop SET 
+		site_id='".$cmysql->real_escape_string($account["site_id"])."',
+		imap_account_id='".$cmysql->real_escape_string($account["imap_account_id"])."',
+		queue_pop_message_uid='".$cmysql->real_escape_string($msg['uid'])."',
+		queue_pop_created_datetime='".$cmysql->real_escape_string(date('Y-m-d H:i:s'))."',
+		queue_pop_updated_datetime='".$cmysql->real_escape_string(date('Y-m-d H:i:s'))."',
+		queue_pop_scheduled_processing_datetime='".$cmysql->real_escape_string(date('Y-m-d H:i:s'))."', 
+		queue_pop_message_json='".$cmysql->real_escape_string($messageJson)."',
+		queue_pop_process_fail_count='".$cmysql->real_escape_string(0)."',
+		queue_pop_process_retry_interval_seconds='".$cmysql->real_escape_string(60)."',
+		queue_pop_deleted='".$cmysql->real_escape_string(0);
+		$cmysql->query($sql, MYSQLI_STORE_RESULT);
 
 	}
 	
