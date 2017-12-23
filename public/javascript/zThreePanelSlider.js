2a955c4eb90fc41502df7a50ab0e01a17e78f68b
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2017-12-23 14:13:13 -0500

    three panel almost done

--- public/javascript/zThreePanelSlider.js
+++ public/javascript/zThreePanelSlider.js
@@ -1,27 +1,68 @@
-// the other dimensions should be options
+// the other dimensions should be options - almost done
+// setup default options
+// test more than one on same page with different settings
 
+/*
+// usage
+*/
 
 (function($, window, document, undefined){
 	"use strict";
 	function zThreePanelSlider(options){
-		var self=this;
-
+		var self=this; 
+		options.arrSlide=zso(options, 'arrSlide', false, []);
+		options.selector=zso(options, 'selector', false, ''); 
+		options.pager=zso(options, 'pager', false, true);
+		options.pagerStyle=zso(options, 'pagerStyle', false, 'circles'); // circles or squares
+		options.auto=zso(options, 'auto', false, true);
+		options.timeout=zso(options, 'timeout', true, 5000); // milliseconds
+		options.touchSwipe=zso(options, 'touchSwipe', false, true);
+		options.nextPrevious=zso(options, 'nextPrevious', false, true); // enable the next / previous buttons to be automatically placed
+		options.nextButton=zso(options, 'nextButton', false, '&#10093;');
+		options.previousButton=zso(options, 'previousButton', false, '&#10092;');
+		options.nextPreviousCenterClass=zso(options, 'nextPreviousCenterClass', false, '');
+		options.slideWidthPercent=zso(options, 'slideWidthPercent', true, 66.66);
+   
 		var animating=false; 
 		var currentSlideIndex=0;
 		var direction=0;
-		var d={
-			slides:$(options.selector+" .threePanelSlider > div"),
-			container:$(options.selector),
-			arrPanelOrder:["threePanelSlide1", "threePanelSlide2", "threePanelSlide3", "threePanelSlide4", "threePanelSlide5"]
-		};
+		var slider=$(options.selector);
+		var arrPanelOrder=[];
+		var slideWidth=0;
+
+		if(slider.length==0){
+			console.log("zThreePanelSlider: options.selector not found: "+options.selector);
+			return;
+		}
+		if(slider.length==0){
+			console.log("zThreePanelSlider: options.arrSlide has no slides");
+			return;
+		}
+		if(options.nextPreviousCenterClass==""){
+			options.nextPreviousCenterClass=options.selector+" .threePanelSlide";
+		}
+
+		slider.append('<div class="threePanelSliderBackground"><div class="threePanelSlider"></div></div>');
 		var slideNameOffset=0;
 		var sliderInterval=false;
 		var pager=false;
 		var firstLoad=true;		
 		var previousButton=false;
 		var nextButton=false;
+		if(options.slideWidthPercent < 33.33){
+		//	alert('options.slideWidthPercent must be at least 33.33.');
+		}
+		var panelCount=Math.round(100/options.slideWidthPercent)+2; 
+		if(panelCount/2 == Math.floor(panelCount/2)){
+			// force to odd number of panels
+			panelCount++;
+		}
+		var middlePanelOffset=Math.floor(panelCount/2);
+		console.log("panelCount:"+panelCount+" middlePanelOffset:"+middlePanelOffset);
 
-		d.slideCount=d.slides.length;
+		var left=(100-options.slideWidthPercent)/2;
+		var firstPercent=Math.round((left-(options.slideWidthPercent*middlePanelOffset))*100)/100;
+ 
 		function init(){
 			reloadSlides(false, 0); 
 			zArrResizeFunctions.push({functionName:resizeSlider}); 
@@ -50,7 +91,7 @@
 
 		function attachTouchSwipe(){
 
-			d.container.on( 'mousedown', function( event ) {
+			slider.on( 'mousedown', function( event ) {
 				event.preventDefault(); 
 				var mousePos = zDrag_mouseCoords(event.originalEvent); 
 				this.lastMousePositionY=mousePos.y; 
@@ -58,7 +99,7 @@
 				//theImageRotator.cancelAutoRotate();  
 				return false;
 			} );
-			d.container.on( 'mousemove', function( event ) { 
+			slider.on( 'mousemove', function( event ) { 
 				var mousePos = zDrag_mouseCoords(event.originalEvent); 
 
 				var differenceY=this.lastMousePositionY-mousePos.y;
@@ -86,16 +127,16 @@
 					return true;
 				}
 			} );
-			d.container.on( 'mouseup mouseout', function( event ) {
+			slider.on( 'mouseup mouseout', function( event ) {
 				event.preventDefault();
 				delete this.lastMousePositionX;
 				return false;
 			} ); 
 
-			d.container.on( 'touchstart', function( event ) {  
+			slider.on( 'touchstart', function( event ) {  
 				this.lastTouchPosition=event.originalEvent;  
 			} );
-			d.container.on( 'touchmove', function( event ) {
+			slider.on( 'touchmove', function( event ) {
 				if(typeof this.lastTouchPosition =="undefined" || typeof this.lastTouchPosition.touches == "undefined"){
 					return;
 				}
@@ -122,7 +163,7 @@
 					return false; 
 				} 
 			} );
-			d.container.on( 'touchend', function( event ) { 
+			slider.on( 'touchend', function( event ) { 
 				delete this.lastTouchPosition;
 				return false;
 			} ); 
@@ -133,7 +174,7 @@
 				$( 'span[data-slide-index="' + currentSlideIndex + '"]', pager ).addClass( 'active' );
 			}
 			$(options.selector+" .threePanelSlide").removeClass("active");
-			$(options.selector+" ."+d.arrPanelOrder[2]).addClass("active");
+			$(options.selector+" ."+arrPanelOrder[middlePanelOffset]).addClass("active");
 		}
 
 		function resetInterval( doAnimation ) {
@@ -148,10 +189,10 @@
 			}
 		}
 		function attachNextPreviousButtons() {
-			d.container.append( '<div class="slider-previous-button">' + options.previousButton + '</div><div class="slider-next-button">' + options.nextButton + '</div>' );
+			slider.append( '<div class="slider-previous-button">' + options.previousButton + '</div><div class="slider-next-button">' + options.nextButton + '</div>' );
 
-			previousButton = $( '.slider-previous-button', d.container );
-			nextButton     = $( '.slider-next-button', d.container ); 
+			previousButton = $( '.slider-previous-button', slider );
+			nextButton     = $( '.slider-next-button', slider ); 
 
 			previousButton.on( 'mousedown touchstart', function( event ) {
 				if((event.type == "mousedown") && event.which!=1){
@@ -186,9 +227,9 @@
 			});
 		}
 		function attachPager() {
-			d.container.append( '<div class="slider-pager ' + options.pagerStyle + '"></div>' );
+			slider.append( '<div class="slider-pager ' + options.pagerStyle + '"></div>' );
 
-			pager = $( '.slider-pager', d.container );
+			pager = $( '.slider-pager', slider );
 
 			for(var i=0;i<options.arrSlide.length;i++){
 				pager.append( '<span data-slide-index="' + i + '"></span>' );
@@ -223,8 +264,8 @@
 			} ); 
 		}
 		function resizeSlider(){
-			d.slideWidth=$(options.selector+" .threePanelSlide > div").width();
-			d.containerWidth=d.container.width();
+			//d.slideWidth=$(options.selector+" .threePanelSlide > div").width();
+			var sliderWidth=slider.width();
 			//console.log(d);
 			var maxHeight=0;
 			$(options.selector+" .threePanelSlide").each(function(){
@@ -234,7 +275,7 @@
 			$(options.selector+" .threePanelSlider").height(maxHeight);
 
 
-			if(!firstLoad && options.nextPrevious && options.nextPreviousCenterClass != ""){ 
+			if(!firstLoad && options.nextPrevious){ 
 				var centerClassHeight=0;
 				$(options.nextPreviousCenterClass).each(function(){
 					centerClassHeight=Math.max(centerClassHeight, zGetAbsPosition(this).height);
@@ -249,23 +290,18 @@
 			if(slideIndex >= options.arrSlide.length){
 				var remainder=slideIndex-(options.arrSlide.length);
 				if(remainder>=options.arrSlide.length){
-					//console.log('branch1 currentSlideIndex:'+currentSlideIndex+' | offset:'+offset);
 					return options.arrSlide.length-1;
 				}else{
-					//console.log('branch2 currentSlideIndex:'+currentSlideIndex+' | offset:'+offset);
 					return remainder;
 				}
 			}else if(slideIndex < 0){ 
 				var remainder=options.arrSlide.length-Math.abs(slideIndex);
 				if(remainder < 0){
-					//console.log('branch3 currentSlideIndex:'+currentSlideIndex+' | offset:'+offset);
 					return options.arrSlide.length-1;
 				}else{
-					//console.log('branch4 currentSlideIndex:'+currentSlideIndex+' | offset:'+offset);
 					return remainder;
 				}
 			}else{
-				//console.log('branch5 currentSlideIndex:'+currentSlideIndex+' | offset:'+offset);
 				return slideIndex;
 			}
 		}
@@ -274,15 +310,16 @@
 			var arrHTML=[];
 			var arrPanelOrderNew=[];
 			slideNameOffset++;
-			for(var i=0;i<5;i++){
-				var loadSlideIndex=getSlideIndex(slideIndex, i-2); 
+			for(var i=0;i<panelCount;i++){
+				var loadSlideIndex=getSlideIndex(slideIndex, i-middlePanelOffset ); 
 				//console.log("reloadSlides loadSlideIndex: "+loadSlideIndex+":"+typeof slideIndex);
 				var slide=options.arrSlide[loadSlideIndex];
 				arrHTML.push('<div class="threePanelSlide threePanelSlide'+slideNameOffset+' threePanelSlide'+slideNameOffset+"_"+(i+1)+' ');
-				if(i == 2){
+				if(i == middlePanelOffset){
 					arrHTML.push(' active');
 				}
-				arrHTML.push('" style="position:absolute; z-index:'+(slideNameOffset+1)+'; left:13%;">'+slide.html+'</div>');
+				left=Math.round(((100-options.slideWidthPercent)/2)*100)/100;
+				arrHTML.push('" style="position:absolute; z-index:'+(slideNameOffset+1)+'; width:'+options.slideWidthPercent+'%; left:'+left+'%;">'+slide.html+'</div>');
 				arrPanelOrderNew.push("threePanelSlide"+slideNameOffset+"_"+(i+1));
 			}
 			$(options.selector+" .threePanelSlider").append(arrHTML.join(""));
@@ -299,13 +336,14 @@
 				} 
 			}); 
 			
-			var firstPercent=-116.66; 
+			//var firstPercent=-116.66; 
+			console.log('firstPercent:'+firstPercent);
 			for(var i=0;i<=arrPanelOrderNew.length-1;i++){
 				var m=arrPanelOrderNew[i];
-				var left=firstPercent+((i)*66.66);  
+				var left=firstPercent+((i)*options.slideWidthPercent);  
 				if(animate){  
 					$(options.selector+" ."+m).css({ 
-						"left":(Math.round((left+(3*66.66*direction))*100)/100)+"%"
+						"left":(Math.round((left+(panelCount*options.slideWidthPercent*direction))*100)/100)+"%"
 					}).animate({
 						"left": (left)+"%"
 					}, 'slow','easeInExpo', function(){
@@ -317,7 +355,7 @@
 					}); 
 				}
 			} 
-			d.arrPanelOrder=arrPanelOrderNew;
+			arrPanelOrder=arrPanelOrderNew;
 
 			$(options.selector+" .threePanelSlide a").on("mousedown touchstart", function(e){
 				if((event.type == "mousedown") && event.which!=1){
@@ -352,8 +390,7 @@
 					arrAnimateQueue.push(slideIndex);
 				}
 			}else{
-				var offset=slideIndex-currentSlideIndex;
-				console.log('queue:'+slideIndex+":"+currentSlideIndex);
+				var offset=slideIndex-currentSlideIndex; 
 				self.animateToSlide(getSlideIndex(currentSlideIndex, offset), offset);
 			}
 		}
@@ -383,26 +420,26 @@
 				//console.log('previousSlide');
 
 				// change panel order
-				var firstPanel=d.arrPanelOrder[0];
-				var lastPanel=d.arrPanelOrder.pop();
-				d.arrPanelOrder.unshift(lastPanel);
+				var firstPanel=arrPanelOrder[0];
+				var lastPanel=arrPanelOrder.pop();
+				arrPanelOrder.unshift(lastPanel);
 				$("."+lastPanel).insertBefore("."+firstPanel); 
 	 
-				//console.log(d.arrPanelOrder);
+				//console.log(arrPanelOrder);
 	 
 				// get the photo index to load
-				var loadSlideIndex=getSlideIndex(slideIndex, -2);
+				var loadSlideIndex=getSlideIndex(slideIndex, -middlePanelOffset);
  
 				//console.log("previous:"+loadSlideIndex);
 				var slide=options.arrSlide[loadSlideIndex];
 				$(options.selector+" ."+lastPanel).html(slide.html);
 				//console.log('loadSlideIndex:'+loadSlideIndex);
 	 
-				var firstPercent=-116.66;
-				for(var i=0;i<d.arrPanelOrder.length;i++){
-					var left=firstPercent+((i)*66.66);  
-					$(options.selector+" ."+d.arrPanelOrder[i]).css({
-						"left":(Math.round((left-66.66)*100)/100)+"%"
+				//var firstPercent=-116.66;
+				for(var i=0;i<arrPanelOrder.length;i++){
+					var left=firstPercent+((i)*options.slideWidthPercent);  
+					$(options.selector+" ."+arrPanelOrder[i]).css({
+						"left":(Math.round((left-options.slideWidthPercent)*100)/100)+"%"
 					}).animate({
 						"left": (left)+"%"
 					}, 'slow','easeInExpo', function(){
@@ -415,26 +452,26 @@
 				//console.log('nextSlide');
 
 				// get the photo index to load
-				var loadSlideIndex=getSlideIndex(slideIndex, 2);
+				var loadSlideIndex=getSlideIndex(slideIndex, middlePanelOffset);
 
 				//console.log("next:"+loadSlideIndex);
 				// change panel order
-				var lastPanel=d.arrPanelOrder[4];
-				var firstPanel=d.arrPanelOrder.shift();
-				d.arrPanelOrder.push(firstPanel);
+				var lastPanel=arrPanelOrder[arrPanelOrder.length-1];
+				var firstPanel=arrPanelOrder.shift();
+				arrPanelOrder.push(firstPanel);
 
 				// get the photo index to load
-				var loadSlideIndex=getSlideIndex(slideIndex, 2);
+				var loadSlideIndex=getSlideIndex(slideIndex, middlePanelOffset);
 
 				var slide=options.arrSlide[loadSlideIndex];
 				// TODO: create img element here and animate onload instead
 				$(options.selector+" ."+firstPanel).html(slide.html);
 
-				firstPercent=-116.66;
-				for(var i=0;i<d.arrPanelOrder.length;i++){
-					var left=firstPercent+((i)*66.66);  
-					$(options.selector+" ."+d.arrPanelOrder[i]).css({
-						"left":(Math.round((left+66.66)*100)/100)+"%"
+				//firstPercent=-116.66;
+				for(var i=0;i<arrPanelOrder.length;i++){
+					var left=firstPercent+((i)*options.slideWidthPercent);  
+					$(options.selector+" ."+arrPanelOrder[i]).css({
+						"left":(Math.round((left+options.slideWidthPercent)*100)/100)+"%"
 					}).animate({
 						"left": (left)+"%"
 					}, 'slow','easeInExpo', function(){
@@ -445,25 +482,21 @@
 				// reload all slides
 				//console.log('reload all slides');  
 				// animate all the existing slides offscreen
-				firstPercent=-116.66;
+				//firstPercent=-116.66;
 				if(offset>0){
 					var direction=1;
 				}else{
 					var direction=-1;
 				}
-				for(var i=0;i<=d.arrPanelOrder.length-1;i++){
-					var left=firstPercent+((i)*66.66);  
-					$(options.selector+" ."+d.arrPanelOrder[i]).css({
-						//"left":(Math.round((left+66.66)*100)/100)+"%"
-					}).animate({
-						"left": (left-(66.66*3*direction))+"%"
+				for(var i=0;i<=arrPanelOrder.length-1;i++){
+					var left=firstPercent+((i)*options.slideWidthPercent);  
+					$(options.selector+" ."+arrPanelOrder[i]).animate({
+						"left": (left-(options.slideWidthPercent*panelCount*direction))+"%"
 					}, 'slow','easeInExpo', function(){ 
 						animating=false;
 						$(this).remove();
 					});
-				} 
-				//return;
-				// probably need to set currentSlideIndex sooner?
+				}  
 				currentSlideIndex=slideIndex;
 				reloadSlides(true, direction); 
 			}
