c276eea06fb31fa72ec6830cb3e129b93314fbed
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2018-08-23 17:31:17 -0400

    made custom php scripts able to be different on test server

--- public/index.php
+++ public/index.php
@@ -14,10 +14,24 @@ if($method=='host-time'){
 }else if($method=='yelpp'){
 	require("a/content/yelpp.php");
 }else{
-	error_reporting(E_ALL ^ E_DEPRECATED);
-	ini_set('display_errors', 1);
-	$allowed=get_cfg_var("jetendo_allowed_php_script_list");
-	$allowedMethods=get_cfg_var("jetendo_allowed_php_method_list");
+
+	function zIsTestServer(){
+		global $isTestServer;
+		return $isTestServer;
+	}
+	$host=$_SERVER["HTTP_HOST"];
+	$testDomain=get_cfg_var("jetendo_test_domain"); 
+	if(strpos($host, $testDomain) !== FALSE){
+		$isTestServer=true; 
+		error_reporting(E_ALL ^ E_DEPRECATED);
+		ini_set('display_errors', 1);
+		$allowed=get_cfg_var("jetendo_test_allowed_php_script_list");
+		$allowedMethods=get_cfg_var("jetendo_test_allowed_php_method_list");
+	}else{
+		$isTestServer=false; 
+		$allowed=get_cfg_var("jetendo_allowed_php_script_list");
+		$allowedMethods=get_cfg_var("jetendo_allowed_php_method_list");
+	} 
 	if($allowed != ""){
 		$arrScript=explode("|", $allowed);
 		$arrMethod=explode("|", $allowedMethods);
