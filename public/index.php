f0e77e07d90b082202cbede94496b7104bb311a1
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2018-08-23 09:38:27 -0400

    added php script add-on feature

--- public/index.php
+++ public/index.php
@@ -14,6 +14,20 @@ if($method=='host-time'){
 }else if($method=='yelpp'){
 	require("a/content/yelpp.php");
 }else{
+	error_reporting(E_ALL ^ E_DEPRECATED);
+	ini_set('display_errors', 1);
+	$allowed=get_cfg_var("jetendo_allowed_php_script_list");
+	$allowedMethods=get_cfg_var("jetendo_allowed_php_method_list");
+	if($allowed != ""){
+		$arrScript=explode("|", $allowed);
+		$arrMethod=explode("|", $allowedMethods);
+		for($i=0;$i<count($arrScript);$i++){ 
+			if($method==$arrMethod[$i]){ 
+				require($arrScript[$i]);
+				exit;
+			}
+		}
+	}
 	header("HTTP/1.0 404 Not Found");
 	exit;
 }
